15ffd3508ac0b488487e7d4b1d87c253b978e99e
miwanoff <marina.iva.noffa@gmail.com>
2024-12-22 12:45:10 +0200

    Add ootstraps blocks

--- index.php
+++ index.php
@@ -15,7 +15,21 @@ if (isset($_POST["go"])) {
     } else {
         echo "You are not registered";
     }
-}
+}?>
+<!-- Page content-->
+<section class="py-5">
+    <div class="container px-5">
+        <!-- Contact form-->
+        <div class="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
+            <div class="text-center mb-5">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i>
+                </div>
+                <h2 class="fw-bolder">Log in</h2>
+                <p class="lead fw-normal text-muted mb-0">We'd love to hear from you</p>
+            </div>
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <?php
 $user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="autoForm">
 <input type="text" name="login" placeholder="Input login">
 <input type="password" name="pass" placeholder="Input password">
@@ -25,8 +39,21 @@ $user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="au
 if (!$autorized) {
     echo $user_form;
 }
-
-$str_form_s = '<h3>Сортировать по:</h3>
+?>
+                </div>
+            </div>
+        </div>
+        <div class="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
+            <div class="text-center mb-5">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i>
+                </div>
+                <h2 class="fw-bolder">Sorting</h2>
+                <p class="lead fw-normal text-muted mb-0">We'd love to hear from you</p>
+            </div>
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <?php
+$str_form_s = '
 <form action="index.php" method="post" name="sort_form">
     <select name="sort" id="sort" size="1">
         <option value="name">назва</option>
@@ -51,16 +78,25 @@ if (isset($_POST["sort"])) {
         echo "Нет данных";
     }
 }
-
-$str_form_search = "
-<div class=\"container\">
-    <h3>Search:</h3>
-    <form name='searchForm' action='index.php' method='post' onSubmit='return overify_login(this);'>
-        <input type='text' name='search' class='form-control'>
-        <input type='submit' name='gosearch' value='Confirm' class='btn btn-secondary my-2'>
-        <input type='reset' name='clear' value='Reset' class='btn btn-secondary my-2'>
-    </form>
-</div>";
+?>
+                </div>
+            </div>
+        </div>
+        <div class="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
+            <div class="text-center mb-5">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-envelope"></i>
+                </div>
+                <h2 class="fw-bolder">Search</h2>
+                <p class="lead fw-normal text-muted mb-0">We'd love to hear from you</p>
+            </div>
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <?php
+$str_form_search = "<form name='searchForm' action='index.php' method='post' onSubmit='return overify_login(this);'>
+                            <input type='text' name='search' class='form-control'>
+                            <input type='submit' name='gosearch' value='Confirm' class='btn btn-secondary my-2'>
+                            <input type='reset' name='clear' value='Reset' class='btn btn-secondary my-2'>
+</form>";
 
 echo $str_form_search;
 
@@ -68,7 +104,7 @@ if (isset($_POST['gosearch'])) {
     $data = test_input($_POST['search']);
     $out = out_search($data);
 
-// виклик функції out_arr() з action.php для отримання массиву
+    // виклик функції out_arr() з action.php для отримання массиву
     if (count($out) > 0) {
         foreach ($out as $row) { //вывод массива построчно
             echo $row;
@@ -77,6 +113,39 @@ if (isset($_POST['gosearch'])) {
     {
         echo "Nothing found...";
     }
-}
+}?> </div>
+            </div>
+        </div>
+        <!-- Contact cards-->
+        <div class="row gx-5 row-cols-2 row-cols-lg-4 py-5">
+            <div class="col">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-chat-dots"></i>
+                </div>
+                <div class="h5 mb-2">Chat with us</div>
+                <p class="text-muted mb-0">Chat live with one of our support specialists.</p>
+            </div>
+            <div class="col">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-people"></i>
+                </div>
+                <div class="h5">Ask the community</div>
+                <p class="text-muted mb-0">Explore our community forums and communicate with other users.</p>
+            </div>
+            <div class="col">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i
+                        class="bi bi-question-circle"></i>
+                </div>
+                <div class="h5">Support center</div>
+                <p class="text-muted mb-0">Browse FAQ's and support articles to find solutions.</p>
+            </div>
+            <div class="col">
+                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-telephone"></i>
+                </div>
+                <div class="h5">Call us</div>
+                <p class="text-muted mb-0">Call us during normal business hours at (555) 892-9403.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
 
 include "footer.php";
